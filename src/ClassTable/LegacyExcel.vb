fe66b285ae364694c2b2f40467c750c2038de1ec
MyBlueHorizon <tianjizhishang@outlook.com>
2021-09-19 20:05:46 +0800

    修正课表加载完成后依然占用文件的问题

--- src/ClassTable/LegacyExcel.vb
+++ src/ClassTable/LegacyExcel.vb
@@ -13,6 +13,7 @@ Public Class LegacyExcel
         Dim TableString As String
         TableString =
     "{" + "RangeA" + ":" + Chr(34) & TableSheet.Cell("A3").Value & Chr(34) + "," + "RangeB" + ":" + Chr(34) & TableSheet.Cell("A4").Value & Chr(34) + "," + "RangeC" + ":" + Chr(34) & TableSheet.Cell("A5").Value & Chr(34) + "," + "RangeD" + ":" + Chr(34) & TableSheet.Cell("A6").Value & Chr(34) + "," + "RangeE" + ":" + Chr(34) & TableSheet.Cell("A7").Value & Chr(34) + "," + "RangeF" + ":" + Chr(34) & TableSheet.Cell("A8").Value & Chr(34) + "," + "RangeG" + ":" + Chr(34) & TableSheet.Cell("A10").Value & Chr(34) + "," + "RangeH" + ":" + Chr(34) & TableSheet.Cell("A11").Value & Chr(34) + "," + "RangeI" + ":" + Chr(34) & TableSheet.Cell("A12").Value & Chr(34) + "," + "RangeJ" + ":" + Chr(34) & TableSheet.Cell("A13").Value & Chr(34) + "," + "RangeK" + ":" + Chr(34) & TableSheet.Cell("A14").Value & Chr(34) + "," + "RangeL" + ":" + Chr(34) & TableSheet.Cell("A15").Value & Chr(34) + "}"
+        fileStream.Close()
         Return TableString
     End Function
 
